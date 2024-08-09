1b9ef1a5fe01cd30fbe1aca8f91b67713a660345
Paolo Rechia <paolorechia@gmail.com>
2024-08-09 22:14:38 +0200

    REmove license notes

--- src/py_dep_graph/__about__.py
+++ src/py_dep_graph/__about__.py
@@ -1,4 +1,2 @@
-# SPDX-FileCopyrightText: 2024-present Paolo Rechia <paolorechia@gmail.com>
-#
-# SPDX-License-Identifier: MIT
+
 __version__ = "0.0.1"
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Paolo Rechia <paolorechia@gmail.com>
-#
-# SPDX-License-Identifier: MIT
