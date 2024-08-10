99f43db654a83b55a2c567a545e8153a0e492595
Paolo Rechia <paolorechia@gmail.com>
2024-08-10 13:42:58 +0200

    Start developing FileTree

---
+++ src/py_dep_graph/filetree.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import os
+
+
+class Node:
+    """Represents an abstract generic node (dir or file)"""
+
+    def __init__(self, name: str, filepath: str) -> None:
+        self.name: str = name
+        self.filepath: str = filepath
+
+
+class DirNode(Node):
+    """Represents a specific directory node in the tree"""
+
+    def __init__(self, name: str, filepath: str, parent: None | DirNode = None) -> None:
+        super().__init__(name, filepath)
+        self.children = []
+        self.parent: None | DirNode = parent
+
+    def add_child(self, node: DirNode | SourceCodeFileNode):
+        self.children.append(node)
+
+    def __repr__(self) -> str:
+        children_repr = [child.__repr__() for child in self.children]
+        children_repr_str = ", ".join(children_repr)
+        return f"DirNode(name={self.name}, children={children_repr_str})"
+
+
+class SourceCodeFileNode(Node):
+    """Represents a specific source code file node in tree"""
+
+    def __init__(self, name: str, filepath: str, parent: DirNode) -> None:
+        super().__init__(name, filepath)
+        self.parent: DirNode = parent
+
+    def __repr__(self) -> str:
+        return f"SourceCodeFileNode(name={self.name})"
+
+
+class FileTree:
+    """Represents the current files in a root directory"""
+
+    def __init__(self, root_directory_filepath: str) -> None:
+        self.root_directory: DirNode = DirNode(root_directory_filepath, root_directory_filepath)
+        iter_ = os.walk(root_directory_filepath)
+        path, directories, files = next(iter_)
+        for dir_ in directories:
+            self.root_directory.add_child(DirNode(dir_, os.path.join(path, dir_), self.root_directory))
+
+        for f in files:
+            if ".py" in f:
+                self.root_directory.add_child(SourceCodeFileNode(f, os.path.join(path, f), self.root_directory))
---
+++ tests/test_filetree.py
@@ -0,0 +1,45 @@
+import os
+import tempfile
+
+from py_dep_graph.filetree import FileTree
+
+
+def walk_dict(path: str, d: dict):
+    for key, item in d.items():
+        # Create directory
+        if isinstance(item, dict):
+            new_dir = os.path.join(path, key)
+            os.mkdir(new_dir)
+            walk_dict(new_dir, item)
+
+        # Create file
+        else:
+            filepath = os.path.join(path, key)
+            with open(filepath, "w") as fp:
+                fp.write("")
+
+
+def create_test_filesystem(tmp_dir_root):
+    dirs = {
+        "pkg1": {"mod1.py": None, "mod2.py": None, "mod3.py": None},
+        "pkg2": {"pkg4": {"mod4.py": None}},
+        "pkg3": {"mod5.py": None},
+        "mod6.py": None,
+    }
+    walk_dict(tmp_dir_root, dirs)
+
+
+def test_filetree():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        create_test_filesystem(tmp_dir)
+
+        file_tree = FileTree(tmp_dir)
+        assert file_tree.root_directory.name == tmp_dir
+        assert file_tree.root_directory.filepath == tmp_dir
+        names = [child.name for child in file_tree.root_directory.children]
+        assert sorted(names) == [
+            "mod6.py",
+            "pkg1",
+            "pkg2",
+            "pkg3",
+        ]
